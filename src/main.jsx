15e845a74282f4a1bf61fceab288f6c6e69d8f0c
Mohammed adachi <mohammed.adachi@uit.ac.ma>
2025-03-25 03:10:23 +0000

    Refactor routing structure to use Layout component for better organization

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from "./components/header/index";
+import Layout from "./components/header/MainLayout";
 import Inscription from "./components/inscription/inscit";
 import List from "./components/list/list";
 import Notification from "./components/notification/notification"
 import App from "./App";
-import Dashbord from "./page/Dashbord/index";
-import { TauriEvent } from "@tauri-apps/api/event";
 import SportMembers from "./components/sport_list/sport_list";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
   <BrowserRouter>
-    <Header> </Header>
     <Routes>
-
-      <Route path="/" element={<App />} />
-      <Route path="/inscription" element={<Inscription />} />
-      <Route path="/List_de_client" element={<List />} />
-      <Route path="/SportMembers" element={<SportMembers />} />
-      <Route path="/Notification" element={<Notification />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<App />} />
+        <Route path="/inscription" element={<Inscription />} />
+        <Route path="/List_de_client" element={<List />} />
+        <Route path="/SportMembers" element={<SportMembers />} />
+        <Route path="/Notification" element={<Notification />} />
+      </Route>
     </Routes>
   </BrowserRouter>
 );
