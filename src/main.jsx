2b380e0140e948c9d40a1d73741c0f4c2d74ec98
Mohammed adachi <mohammed.adachi@uit.ac.ma>
2025-03-26 02:51:20 +0000

    Fix import name for List component and update route path to use Lists

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from "./components/header/MainLayout";
 import Inscription from "./components/inscription/inscit";
-import List from "./components/list/list";
+import Lists from "./components/list/list";
 import Notification from "./components/notification/notification"
 import App from "./App";
 import SportMembers from "./components/sport_list/sport_list";
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Route element={<Layout />}>
         <Route path="/" element={<App />} />
         <Route path="/inscription" element={<Inscription />} />
-        <Route path="/List_de_client" element={<List />} />
+        <Route path="/List_de_client" element={<Lists />} />
         <Route path="/SportMembers" element={<SportMembers />} />
         <Route path="/Notification" element={<Notification />} />
       </Route>
